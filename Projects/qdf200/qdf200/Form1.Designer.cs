40723b4a1761f072ec196b7264430b45f961aaee
yallawalla <mr.mocnik@gmail.com>
2016-07-15 16:20:21 +0200

    ....qdf200

---
+++ Projects/qdf200/qdf200/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace qdf200
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.com = new System.IO.Ports.SerialPort(this.components);
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Connect = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.menu.Dock = System.Windows.Forms.DockStyle.None;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.Connect});
+            this.menu.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
+            this.menu.Location = new System.Drawing.Point(483, 9);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(70, 44);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            this.menu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(63, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // Connect
+            // 
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(63, 19);
+            this.Connect.Text = "Connect";
+            this.Connect.DropDownOpening += new System.EventHandler(this.Connect_DropDownOpening);
+            this.Connect.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Connect_DropDownItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 296);
+            this.Controls.Add(this.menu);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort com;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Connect;
+    }
+}
+
---
+++ Projects/qdf200/qdf200/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace qdf200
+{
+    public partial class Form1 : Form
+    {
+        Thread rx = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                Console.WriteLine(port);
+            
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Connect_DropDownOpening(object sender, EventArgs e)
+        {
+            Connect.DropDownItems.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                if(port.IndexOf("ACM")>0 || port.IndexOf("USB")>0)
+                    Connect.DropDownItems.Add(new ToolStripMenuItem(port));
+        }
+
+        private void Connect_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Text = "...connecting";
+//            Properties.wtf.Default.port = e.ClickedItem.Text;
+//            Properties.wtf.Default.Save();
+            if(com.IsOpen)
+            {
+                com.Close();
+                rx.Abort();
+            }
+            string s = e.ClickedItem.Text;
+            int n = s.IndexOf("CO");
+            s = s.Substring(s.IndexOf("tty"));
+            try { 
+                com.PortName = s;
+                com.Open();
+                rx = new Thread(new ThreadStart(rxData));
+                rx.Start();
+            }
+            catch
+            {
+                Text = s + " not connected ...";
+            }
+ 
+        }
+
+        void rxData()
+        {
+            byte tmpByte = (byte)com.ReadByte();
+            Console.Write("start...");
+            while (true)
+            {
+                string rxString = "";
+                while (tmpByte != 255)
+                {
+                    rxString += ((char)tmpByte);
+                    tmpByte = (byte)com.ReadByte();
+                }
+                if(rxString != "")
+                    Console.Write(rxString);
+                Thread.Sleep(5);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (com.IsOpen == true)
+            {
+                com.Write(e.KeyChar.ToString());
+            }
+        }
+
+    }
+}
