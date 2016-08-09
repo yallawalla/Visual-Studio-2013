5ea749d66432500e3b26ab4d9b1b4435d2ba4115
matjaz <mr.mocnik@gmail.com>
2016-08-09 09:28:42 +0200

    canwr...

--- Projects/canwr/canwr/Program.cs
+++ Projects/canwr/canwr/Program.cs
@@ -16,30 +16,37 @@ namespace canwr
         [DllImport("./cancan/bin/Debug/libcancan.so", EntryPoint = "can_init")]
         private static extern int can_init(int[] filter, int[] mask, int n);
         [DllImport("./cancan/bin/Debug/libcancan.so", EntryPoint = "can_write")]
-        private static extern int can_write(int id, char[] data, int n);
+        private static extern int can_write(int id, byte[] data, int n);
         [DllImport("./cancan/bin/Debug/libcancan.so", EntryPoint = "can_read")]
-        private static extern int can_read(char[] data, int n);
+        private static extern int can_read(byte[] data, int n);
 
         static void Main(string[] args)
         {
-            int n;
+
             string s;
-            char[]  buf = new char[16];
+            byte[]  buf = new byte[1600];
             int[]   filters = new int[] {0x21,0x41,0xb0,0xb1};
             int[]   masks = new int[] {0xff,0xff,0xff,0xff};
-            char[]  data = new char[16];
-              can_init(filters,masks,4);
+            byte[] data = new byte[16];
+            byte[] bb = new byte[16];
+            if(can_init(filters, masks, 4) != 0)
+                Console.WriteLine("init. error...");
             do
             {
                 if (Console.KeyAvailable == true)
                 {
-                    data[0] = (char)Console.ReadKey().Key;
+                    data[0] = (byte)Console.ReadKey().Key;
                     can_write(0x21, data, 1);
                 }
-                n = can_read(buf, 16);
+                Thread.Sleep(300);
+                Console.WriteLine(".");
+                int n = can_read(buf, buf.Length);
                 if (n > 0)  {
-                    Console.Write(buf);
-                    Thread.Sleep(10);
+                    for (int i = 0; i < n; i += 16)
+                    {
+                        Array.Copy(buf, i, bb, 0, 16);
+                        Console.WriteLine(BitConverter.ToString(bb));
+                   }
                 }
             } while (true);
 
