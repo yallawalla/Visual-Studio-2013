8764217d63c148af4da40f0bad038b2bfff04e0d
MOCNIK <matjaz.mocnik@fotona.com>
2021-09-08 10:34:32 +0200

    ...

---
+++ Projects/rk/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace rk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.hScrollBar4 = new System.Windows.Forms.HScrollBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.hScrollBar5 = new System.Windows.Forms.HScrollBar();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.hScrollBar6 = new System.Windows.Forms.HScrollBar();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.hScrollBar7 = new System.Windows.Forms.HScrollBar();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.hScrollBar8 = new System.Windows.Forms.HScrollBar();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(130, 9);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar1.TabIndex = 2;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(130, 35);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(103, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(130, 61);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar3.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(103, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // hScrollBar4
+            // 
+            this.hScrollBar4.Location = new System.Drawing.Point(130, 87);
+            this.hScrollBar4.Name = "hScrollBar4";
+            this.hScrollBar4.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar4.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 86);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // hScrollBar5
+            // 
+            this.hScrollBar5.Location = new System.Drawing.Point(130, 113);
+            this.hScrollBar5.Name = "hScrollBar5";
+            this.hScrollBar5.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar5.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 112);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(103, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // hScrollBar6
+            // 
+            this.hScrollBar6.Location = new System.Drawing.Point(130, 139);
+            this.hScrollBar6.Name = "hScrollBar6";
+            this.hScrollBar6.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar6.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 138);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(103, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // hScrollBar7
+            // 
+            this.hScrollBar7.Location = new System.Drawing.Point(130, 165);
+            this.hScrollBar7.Name = "hScrollBar7";
+            this.hScrollBar7.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar7.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 164);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(103, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // hScrollBar8
+            // 
+            this.hScrollBar8.Location = new System.Drawing.Point(130, 191);
+            this.hScrollBar8.Name = "hScrollBar8";
+            this.hScrollBar8.Size = new System.Drawing.Size(292, 19);
+            this.hScrollBar8.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 190);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(103, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 216);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(662, 373);
+            this.chart1.TabIndex = 17;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(692, 598);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.hScrollBar8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.hScrollBar7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.hScrollBar6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.hScrollBar5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.hScrollBar4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.HScrollBar hScrollBar4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.HScrollBar hScrollBar5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.HScrollBar hScrollBar6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.HScrollBar hScrollBar7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.HScrollBar hScrollBar8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
---
+++ Projects/rk/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rk
+{
+    public struct  atm {
+        public double temp;             // izmerjena temperatura
+        public double hg;               // izmerjeni pritisk
+        public double rv;               // izmerjena rel. vlaga
+        public double h;                // višina meritve
+        public double tau0;             /* temperatura na povrsini zemlje */
+
+        public double ha0;              /* pritisk na povrsini zemlje v mm Hg */
+        public double ro0;
+        public double a0;
+        public double ro0a0;
+        public double y;                /* visina nad povrsino zemlje */
+        public double tau;              /* temperatura */
+        public double hm;               /* pritisk v mm Hg */
+        public double ro;	    	    /* gostota */
+        public double a;    	        /* hitrost zvoka */
+    };
+
+    public partial class Form1 : Form
+    {
+        atm atm;
+        double[] emji = new double[] {
+	        0.77, 0.85, 0.94, 1.03, 1.13, 1.24, 1.36, 1.49,
+	        1.63, 1.78, 1.95, 2.13, 2.32, 2.53, 2.76, 3.01,
+	        3.29, 3.57, 3.88, 4.22, 4.58, 4.90, 5.30, 5.70,
+	        6.10, 6.50, 7.00, 7.50, 8.10, 8.60, 9.20, 9.80,
+	        10.5, 11.2, 12.0, 12.8, 13.6, 14.5, 15.5, 16.5,
+	        17.5, 18.7, 19.8, 21.1, 22.4, 23.8, 25.2, 26.7,
+	        28.4, 30.0, 31.8, 33.7, 35.7, 37.7, 39.9, 42.2,
+	        44.6, 47.1, 49.7, 52.4, 55.3, 55.3};
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atm.rv = 60;
+            atm.hg = 750;
+            atm.temp = 25;
+            atm.h = 0;
+
+            int i = (int)Math.Max(0, Math.Min(60, atm.temp + 20));
+            double em = emji[i] + (atm.temp - Math.Floor(atm.temp)) * (emji[i + 1] - emji[i]);
+            atm.tau = (273.0 + atm.temp) / (1.0 - (3.0 * atm.rv * em) / (800.0 * atm.hg));
+            atm.a = 20.0484413 * Math.Sqrt(atm.tau);
+            atm.ro = 0.4643761 * atm.hg / atm.tau;
+            for (int k = 0; k < 10000; k += 1000)
+            {
+                chart1.Series.Add("s" + k.ToString());
+                chart1.Series["s" + k.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                for (int n = 100; n < 1000; n += 10)
+                    chart1.Series["s" + k.ToString()].Points.AddXY(n, drag(n, k));
+            }
+        }
+
+        private double drag(double v, double h)
+        {
+            double tau = atm.tau - 0.006328 * (h-atm.h);
+            double hg = 750.0 * Math.Pow(atm.tau / 288.9, 5.4);
+            double a = 20.0484413 * Math.Sqrt(tau);
+            double ro = 0.4643761 * hg / tau;
+            return ro*CD43(v / a);
+        }
+
+        /**************************************************/
+        /*   Siacci izracuna CDS(m). m..Machovo stevilo   */
+        /**************************************************/
+        double Siacci(double m)
+        {
+            double p, r;
+
+            p = 15.06 * m * (340.8 * m - 300.0);
+            p = p / (371.0 + Math.Pow(1.704 * m, 10.0));
+            r = 56.16 * m - 47.95;
+            p = p + Math.Sqrt(r * r + 9.6);
+            p = p + 68.23 * m - 48.05;
+            p = 0.018193 * p / (m * m);
+            return (p);
+        }
+        /**************************************************/
+        /*   CD43(M) izracuna ruski etalon iz leta 1943   */
+        /**************************************************/
+
+        private double CD43(double m)
+        {
+           double[] a1 = new double[] {-2.083333, 1.180556, -0.3125, 0.038056, -0.001667, 0.157};
+           double[] a2 = new double[] {613.333335, -233.33334, 39.0, -2.813333, 0.068667, 0.158};
+           double[] a4 = new double[] {-78.411093, 43.245043, 1.803950, -3.74780, 0.88697, 0.325};
+           double[] a5 = new double[] {1829.806015, -426.848831, 42.480376, -1.984508, 0.015829, 0.385};
+           double[] a6 = new double[] {0.030874, -0.117636, 0.209691, -0.151862, -0.05038, 0.385};
+           double cd, sm;
+           int i;
+
+           if (m < 0.6) {
+              cd = 1.388889;
+              sm = m;
+              for (i = 0; i < 6; i++)
+                cd = cd * sm + a1[i];
+           } else if (m < 0.9) {
+              cd = -533.333337;
+              sm = m - 0.6;
+                for (i = 0; i < 6; i++)
+	         cd = cd * sm + a2[i];
+           } else if (m < 1.0) {
+              cd = -1.025 + 1.35 * m;
+           } else if (m < 1.2) {
+              cd = -445.15561;
+              sm = m - 1.0;
+              for (i = 0; i < 6; i++)
+	         cd = cd * sm + a4[i];
+           } else if (m < 1.4) {
+              cd = -2831.379493;
+              sm = m - 1.2;
+                for (i = 0; i < 6; i++)
+	         cd = cd * sm + a5[i];
+           } else if (m < 3.56) {
+              cd = -0.003049;
+	          sm = m - 1.2;
+                for (i = 0; i < 6; i++)
+	         cd = cd * sm + a6[i];
+           } else {
+	          cd = 0.260;
+           }
+           return cd;
+        }
+
+    }
+}
