a47bb576b8f21f9b543980b04be91c7a49216ec4
MOCNIK <matjaz.mocnik@fotona.com>
2021-09-16 16:30:39 +0200

    ...

--- Projects/emul/emul.cpp
+++ Projects/emul/emul.cpp
@@ -5,16 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <stdlib.h>
+#include <time.h> 
 
-#define nBytes	8
+#define nBits	10
 #define nStop	2
-#define nBaud	100
+#define nBaud	1000
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char c[32],co;
-	int	t = 58917, n = 0, tn[200],data;
+	char c[32];
+	int	t, n = 0, tn[200], data, co, cnt;
 	gets_s(c);
+
+	srand(time(NULL));
+	t = rand() % 0xffff;
 
 	int	stopb = 0;
 	for (int i = 0; i < nStop; ++i)
@@ -22,36 +27,31 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	co = 1;
 	for (int i = 0; c[i]; ++i) {
-		data = c[i] * 2 + (stopb * 2 << nBytes);
-		for (int j = 0; j < nBytes + nStop+1; ++j) {
+		data = c[i] * 2 + (stopb * 2 << nBits);
+		for (int j = 0; j < nBits + nStop+1; ++j) {
 			if ((data & 1) != co) {
 				co ^= 1;
 				tn[n++] = t;
 			}
 			data /= 2;
-			t += nBaud;
+			t += nBaud + rand() % nBaud/10 - nBaud / 10 / 2;
 		}
 	}
 
 
-	co = data = 0;
+
+	co = ~((1 << nBits)-1);
+	data = cnt = 0;
 	for (int i = 1; i < n; ++i) {
-		int dt = (tn[i] - tn[i - 1] + nBaud / 2) / nBaud;
-		for (int j = 0; j < dt; ++j)
-			data = 2 * data + co;
-		co ^= 1;
+		cnt += ((tn[i] - tn[i - 1] + nBaud / 2) / nBaud);
+		if (cnt % (nBits + nStop + 1) == 0)
+			printf("%c", data % ((1 << nBits) - 1));
+		data = data >> ((tn[i] - tn[i - 1] + nBaud / 2) / nBaud);
+		data ^= co;
 	}
-
-
-
-
-
-
-
-	for (int i = 0; i < n; ++i)
-		printf("%5d\r\n", tn[i]);
-
+	printf("%c", data % ((1 << nBits) - 1));
 	printf("\r\n");
+	while (!_kbhit());
 	return 0;
 }
 
