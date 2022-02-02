346312e9e0cb2e6cb7819952da44931b956cc19c
MOCNIK <matjaz.mocnik@fotona.com>
2022-02-02 16:33:53 +0100

    ...

--- Projects/slam/slam/Form1.cs
+++ Projects/slam/slam/Form1.cs
@@ -13,23 +13,61 @@ namespace slam
 {
     public struct Vehicle
     {
-        public double X, Y, Vx, Vy, fi, V, theta;
+        public double X, Y, Vx, Vy, fi, V, theta,lambda;
+        public void Draw(Graphics g)
+        {
+            Rectangle r = new Rectangle( (int)X-10, (int)Y-5, 20, 10 );
+            using (Matrix m = new Matrix())
+            {
+                m.RotateAt((float)(fi/Math.PI*180), new PointF(r.Left + (r.Width / 2), r.Top + (r.Height / 2)));
+                g.Transform = m;
+                g.DrawRectangle(Pens.Red, r);
+                g.ResetTransform();
+            }
+
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddPie((int)X - 100, (int)Y - 100, 200, 200, (int)(lambda/Math.PI*180)-10,20);
+            PathGradientBrush pgb = new PathGradientBrush(gp);
+            pgb.CenterPoint = new Point((int)X,(int)Y);
+            pgb.CenterColor = Color.White;
+            pgb.SurroundColors = new Color[] { Color.Black };
+
+            g.FillPath(pgb, gp);
+
+            pgb.Dispose();
+            gp.Dispose();
+        }
+
+        public void Update()
+        {
+            Vx = V * Math.Cos(theta);
+            Vy = V * Math.Sin(theta);
+            X += Vx;
+            Y += Vy;
+            fi = Math.Atan2(Vy, Vx);
+        }
     }
 
     public partial class Form1 : Form
     {
-
-        List<Point> Pos = new List<Point>();
-        List<Point> Echo = new List<Point>();
-        Vehicle Dron;
-        Form Form2=null;
-        Point cp;
+        Random      rnd = new Random();
+        List<Point> Pos = new List<Point>(),  
+                    Echo = new List<Point>();
+        Vehicle     Dron, DronEst;
+        Form        Form2=null;
+        Point       cp;
 
         public Form1()
         {
             InitializeComponent();
             Cursor = Cursors.Hand;
             timer1.Enabled = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cp.X = e.X;
+            cp.Y = e.Y;
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
@@ -49,6 +87,10 @@ namespace slam
                 Dron.X = e.X;
                 Dron.Y = e.Y;
 
+                DronEst = new Vehicle();
+                DronEst.X = Width / 2;
+                DronEst.Y = Height / 2;
+
                 Form2=new Form1();
                 Form2.Show();
                 Form2.Width = Width;
@@ -59,79 +101,47 @@ namespace slam
             }
         }
 
-        //private void label1_Paint(object sender, PaintEventArgs e)
-        //{
-        //    GraphicsPath gp = new GraphicsPath();
-        //    gp.AddEllipse(label1.ClientRectangle);
-
-        //    PathGradientBrush pgb = new PathGradientBrush(gp);
-
-        //    pgb.CenterPoint = new PointF(label1.ClientRectangle.Width / 2,
-        //                                 label1.ClientRectangle.Height / 2);
-        //    pgb.CenterColor = Color.White;
-        //    pgb.SurroundColors = new Color[] { Color.Red };
-
-        //    e.Graphics.FillPath(pgb, gp);
-
-        //    pgb.Dispose();
-        //    gp.Dispose();
-        //}
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
             foreach (Point p in Pos)
-            {
                 e.Graphics.DrawEllipse(Pens.Yellow, p.X - 5, p.Y - 5, 10, 10);
-            }
             if (Form2 != null)
-            {
-                e.Graphics.DrawEllipse(Pens.Green, (int)Dron.X - 5, (int)Dron.Y - 5, 10, 10);
-                e.Graphics.DrawLine(Pens.Green, (float)(Dron.X + 10 * Math.Cos(Dron.fi)), (float)(Dron.Y + 10 * Math.Sin(Dron.fi)), (float)(Dron.X - 30 * Math.Cos(Dron.fi)), (float)(Dron.Y - 30 * Math.Sin(Dron.fi)));
-                e.Graphics.DrawLine(Pens.Red, (float)(Dron.X + 1 * Math.Cos(Dron.theta)), (float)(Dron.Y + 1 * Math.Sin(Dron.theta)), (float)(Dron.X - 10 * Math.Cos(Dron.theta)), (float)(Dron.Y - 10 * Math.Sin(Dron.theta)));
-            }
+                Dron.Draw(e.Graphics);
         }
 
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
             foreach (Point p in Echo)
-            {
                 e.Graphics.DrawEllipse(Pens.LightGray, p.X - 5, p.Y - 5, 10, 10);
-            }
+            DronEst.Draw(e.Graphics);
+            Form2.Text = DronEst.fi.ToString();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
             Refresh();
 
             if (Form2 != null)
             {
                 Dron.V = (Math.Pow(cp.X - Dron.X, 2) + Math.Pow(cp.Y - Dron.Y, 2)) * 0.0001;
                 Dron.theta = Math.Atan2(cp.Y-Dron.Y, cp.X-Dron.X);
-                Dron.Vx = Dron.V * Math.Cos(Dron.theta);
-                Dron.Vy = Dron.V * Math.Sin(Dron.theta);
- 
-                Dron.X += Dron.Vx;
-                Dron.Y += Dron.Vy;
-                Dron.fi = Math.Atan2(Dron.Vy, Dron.Vx);
+                Dron.Update();
 
+                DronEst.V = Dron.V;
+                DronEst.theta = Dron.theta;
+                DronEst.Update();
 
-                Echo.Clear();
                 foreach (Point p in Pos)
                 {
-                    double lambda = Math.Atan2(p.Y - Dron.Y, p.X - Dron.X);
-                    double range = Math.Sqrt(Math.Pow(p.Y - Dron.Y, 2)+Math.Pow(p.X - Dron.X,2));
-
-                    Echo.Add(new Point((int)(range*Math.Cos(lambda-Dron.fi))+Width/2,(int)(range*Math.Sin(lambda-Dron.fi))+Height/2));
+                    double range = Math.Sqrt(Math.Pow(p.Y - Dron.Y, 2) + Math.Pow(p.X - Dron.X, 2));
+                    double lambda = Math.Atan2(p.Y - Dron.Y, p.X - Dron.X - Dron.fi);
+                    if (Math.Abs(Dron.lambda - lambda) < Math.PI/18 && range < 100)
+                        Echo.Add(new Point((int)(range * Math.Cos(Dron.lambda + DronEst.fi) + DronEst.X), (int)(range * Math.Sin(Dron.lambda + DronEst.fi) + DronEst.Y)));
                 }
+                Dron.lambda = (Dron.lambda + Math.PI / 18) % (2*Math.PI);
                 Form2.Refresh();
             }
-        }
-
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            cp.X = e.X;
-            cp.Y = e.Y;
         }
     }
 }
