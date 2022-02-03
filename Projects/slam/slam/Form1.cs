cc1c1cca298ce16d5f66854d4a443cd7ef846ea2
MOCNIK <matjaz.mocnik@fotona.com>
2022-02-03 15:56:40 +0100

    ...

--- Projects/slam/slam/Form1.cs
+++ Projects/slam/slam/Form1.cs
@@ -14,19 +14,20 @@ namespace slam
     public struct Vehicle
     {
         public double X, Y, Vx, Vy, fi, V, theta,lambda;
-        public void Draw(Graphics g)
+        public void Draw(Graphics g, Pen p)
         {
             Rectangle r = new Rectangle( (int)X-10, (int)Y-5, 20, 10 );
             using (Matrix m = new Matrix())
             {
                 m.RotateAt((float)(fi/Math.PI*180), new PointF(r.Left + (r.Width / 2), r.Top + (r.Height / 2)));
                 g.Transform = m;
-                g.DrawRectangle(Pens.Red, r);
+                g.DrawRectangle(p, r);
                 g.ResetTransform();
             }
-
+        }
+        public void Beam(Graphics g, int w, int l) {
             GraphicsPath gp = new GraphicsPath();
-            gp.AddPie((int)X - 100, (int)Y - 100, 200, 200, (int)(lambda/Math.PI*180)-10,20);
+            gp.AddPie((int)X - l/2, (int)Y - l/2, l, l, (int)(lambda/Math.PI*180)-w/2,w);
             PathGradientBrush pgb = new PathGradientBrush(gp);
             pgb.CenterPoint = new Point((int)X,(int)Y);
             pgb.CenterColor = Color.White;
@@ -37,7 +38,6 @@ namespace slam
             pgb.Dispose();
             gp.Dispose();
         }
-
         public void Update()
         {
             Vx = V * Math.Cos(theta);
@@ -83,21 +83,23 @@ namespace slam
 
             if (e.Button == MouseButtons.Right)
             {
-                Dron = new Vehicle();
-                Dron.X = e.X;
-                Dron.Y = e.Y;
+                Dron            = new Vehicle();
+                Dron.X          = e.X;
+                Dron.Y          = e.Y;
 
-                DronEst = new Vehicle();
-                DronEst.X = Width / 2;
-                DronEst.Y = Height / 2;
+                DronEst         = new Vehicle();
+                DronEst.X       = Width / 2;
+                DronEst.Y       = Height / 2;
 
-                Form2=new Form1();
+                Form2           =new Form1();
                 Form2.Show();
-                Form2.Width = Width;
-                Form2.Height = Height;
-                Form2.Left = Left + Width;
-                Form2.Top = Top;
-                Form2.Paint += new PaintEventHandler(Form2_Paint);
+                Form2.Width     = Width;
+                Form2.Height    = Height;
+                Form2.Left      = Left + Width;
+                Form2.Top       = Top;
+                Form2.Paint     += new PaintEventHandler(Form2_Paint);
+
+                Echo.Clear();
             }
         }
 
@@ -107,23 +109,37 @@ namespace slam
             foreach (Point p in Pos)
                 e.Graphics.DrawEllipse(Pens.Yellow, p.X - 5, p.Y - 5, 10, 10);
             if (Form2 != null)
-                Dron.Draw(e.Graphics);
+            {
+                Dron.Draw(e.Graphics, Pens.Red);
+                Dron.Beam(e.Graphics,30,200);
+            }
         }
 
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
             foreach (Point p in Echo)
-                e.Graphics.DrawEllipse(Pens.LightGray, p.X - 5, p.Y - 5, 10, 10);
-            DronEst.Draw(e.Graphics);
-            Form2.Text = DronEst.fi.ToString();
+                e.Graphics.DrawEllipse(Pens.LightGray, p.X - 2, p.Y - 2, 4, 4);
+            DronEst.Draw(e.Graphics, Pens.Yellow);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Refresh();
-
             if (Form2 != null)
             {
+                foreach (Point p in Pos)
+                {
+                    double range = Math.Sqrt(Math.Pow(p.Y - Dron.Y, 2) + Math.Pow(p.X - Dron.X, 2));
+                    double l = Math.Atan2(p.Y - Dron.Y, p.X - Dron.X) - Dron.fi - Dron.lambda;
+
+                    if (l > Math.PI)
+                        l -= 2 * Math.PI;
+                    if (l < -Math.PI)
+                        l += 2 * Math.PI;
+
+                    if (Math.Abs(l) < Math.PI/18.0/2.0 && range < 200)
+                        Echo.Add(new Point((int)(range * Math.Cos(Dron.lambda + DronEst.fi) + DronEst.X), (int)(range * Math.Sin(Dron.lambda + DronEst.fi) + DronEst.Y)));
+                }
+
                 Dron.V = (Math.Pow(cp.X - Dron.X, 2) + Math.Pow(cp.Y - Dron.Y, 2)) * 0.0001;
                 Dron.theta = Math.Atan2(cp.Y-Dron.Y, cp.X-Dron.X);
                 Dron.Update();
@@ -131,17 +147,11 @@ namespace slam
                 DronEst.V = Dron.V;
                 DronEst.theta = Dron.theta;
                 DronEst.Update();
+                Dron.lambda = (Dron.lambda + Math.PI / 18.0) % (2*Math.PI);
 
-                foreach (Point p in Pos)
-                {
-                    double range = Math.Sqrt(Math.Pow(p.Y - Dron.Y, 2) + Math.Pow(p.X - Dron.X, 2));
-                    double lambda = Math.Atan2(p.Y - Dron.Y, p.X - Dron.X - Dron.fi);
-                    if (Math.Abs(Dron.lambda - lambda) < Math.PI/18 && range < 100)
-                        Echo.Add(new Point((int)(range * Math.Cos(Dron.lambda + DronEst.fi) + DronEst.X), (int)(range * Math.Sin(Dron.lambda + DronEst.fi) + DronEst.Y)));
-                }
-                Dron.lambda = (Dron.lambda + Math.PI / 18) % (2*Math.PI);
                 Form2.Refresh();
-            }
+            }  
+            Refresh();
         }
     }
 }
